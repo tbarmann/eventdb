6e0ce3026861d49f6a67b753bd2b8bfa5f445cba
Timothy Barmann <tbarmann92@gmail.com>
2016-10-09 23:30:08 -0400

    Fix rake task importing venues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010031140) do
+ActiveRecord::Schema.define(version: 20161009201401) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "name"
--- lib/tasks/eventdb.rake
+++ lib/tasks/eventdb.rake
@@ -1,6 +1,6 @@
 namespace :db do
   desc "Import venues"
-  task :import_venues do
+  task :import_venues => :environment do
     File.open("venues.json", "r") do |file|
       file.each do |line|
         venue_attrs = JSON.parse line
